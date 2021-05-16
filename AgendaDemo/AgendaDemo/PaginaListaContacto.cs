37c58aa85438329780b6f09487204434104c7e6b
Douglas Juarez <dfjuarezreyes21@gmail.com>
2021-05-16 10:04:51 -0600

    Agregar archivos de proyecto.

---
+++ AgendaDemo/AgendaDemo/PaginaListaContacto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AgendaDemo
+{
+    public class PaginaListaContacto : MasterDetailPage
+    {
+        public PaginaListaContacto()
+        {
+            var lista = new ListView();
+            lista.ItemsSource =
+                GeneradorDeContactos.
+                CrearContactos().
+                OrderBy(x => x.Nombre);
+
+            lista.ItemSelected += (sender, e) =>
+            {
+                if (e.SelectedItem != null)
+                {
+                    Detail = new PaginaContacto(e.SelectedItem as Contacto);
+                    IsPresented = false;
+                }
+            };
+
+            Master = new ContentPage
+            {
+                Title = "Contacto",
+                Content = lista
+            };
+
+            Detail = new PaginaContacto(GeneradorDeContactos.CrearContactos().First());
+        }
+    }
+}
